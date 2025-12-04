6cfacf8afd3d1c4416998978c748807ecb43dc17
Sohambasu07 <soham.basu07@gmail.com>
2025-12-04 23:36:31 +0100

    Main run script

---
+++ bayesian_optimization/__main__.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+import argparse
+import logging
+
+from bayesian_optimization.automl import AutoML
+
+main_logger = logging.getLogger(__name__)
+main_logger.setLevel(logging.INFO)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--num_iterations", "-n",
+        type=int,
+        default=10,
+        help="Number of function evaluations for Bayesian Optimization "
+        "(counted including initial design).",
+    )
+    parser.add_argument(
+        "--initial_design_size", "-i",
+        type=int,
+        default=3,
+        help="Number of initial design points to sample.",
+    )
+    parser.add_argument(
+        "--train_val_split",
+        type=float,
+        default=0.8,
+        help="Train-validation split ratio.",
+    )
+    parser.add_argument(
+        "--seed", "-s",
+        type=int,
+        default=0,
+        help="Random seed for reproducibility.",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cpu",
+        choices=["cpu", "cuda"],
+        help="Device to use for model training and evaluation.",
+    )
+    parser.add_argument(
+        "--objective",
+        type=str,
+        choices=["val_loss", "val_accuracy"],
+        default="val_loss",
+        help="The objective metric name.",
+    )
+    parser.add_argument(
+        "--num_restarts", "-r",
+        type=int,
+        default=20,
+        help="Number of restarts for the GP.",
+    )
+    parser.add_argument(
+        "--batch_size", "-b",
+        type=int,
+        default=64,
+        help="Batch size for training.",
+    )
+
+    args = parser.parse_args()
+
+    automl = AutoML(
+        num_iterations=args.num_iterations,
+        initial_design_size=args.initial_design_size,
+        train_val_split=args.train_val_split,
+    )
+
+    automl.optimize(
+        objective=args.objective,
+        device=args.device,
+        seed=args.seed,
+        hp_target="learning_rate",
+        batch_size=args.batch_size,
+        num_restarts=args.num_restarts,
+    )
